e490cca4e753304da7ec67516fcec72a28e3fcc3
jordnkr <jordnkr408@gmail.com>
2018-01-06 17:47:54 -0600

    adding more demo data
    adding demo data

--- js/dailyResults.js
+++ js/dailyResults.js
@@ -3,22 +3,30 @@ jQuery(document).ready(function($) {
         paging: false
     });
 
-    $('#resultModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var result = button.data('result') // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this)
-        modal.find('.modal-title').text('Test Result')
-        modal.find('.modal-body').text(result)
+    $('#testModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var testName = button.data('testname'); // Extract info from data-* attributes
+        var testClass = button.data('testclass');
+        var namespace = button.data('namespace');
+        //var bugs = MAKE CALL TO GET BUGS // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+
+        // Update the modal's content
+        var modal = $(this);
+        modal.find('.modal-title').text('Test - ' + testName);
+        modal.find('#modalTestClass').text(testClass);
+        modal.find('#modalTestNamespace').text(namespace);
     });
 
-    $('#testModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var testName = button.data('testname') // Extract info from data-* attributes
+    $('#resultModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var testName = button.data('testname');
+        var result = button.data('result');
+        var executionTime = button.data('execution');
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this)
-        modal.find('.modal-title').text('Test - ' + testName)
+        var modal = $(this);
+        modal.find('.modal-title').text('Test - ' + testName);
+        modal.find('#modalResult').text(result);
+        modal.find('#modalTestExecutionTime').text(executionTime);
     });
 });
