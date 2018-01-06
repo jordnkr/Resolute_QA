10b71e58f88ec94ad43755e714f9c985c03ee3ca
jordnkr <jordnkr408@gmail.com>
2018-01-06 00:21:57 -0600

    Upgrading bootstrap and adding modals
    Upgraded to bootstrap 4 beta 3 and added some modals

--- js/dailyResults.js
+++ js/dailyResults.js
@@ -3,4 +3,22 @@ jQuery(document).ready(function($) {
         paging: false
     });
 
+    $('#resultModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var result = button.data('result') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('Test Result')
+        modal.find('.modal-body').text(result)
+    });
+
+    $('#testModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var testName = button.data('testname') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('Test - ' + testName)
+    });
 });
