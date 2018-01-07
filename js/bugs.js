784a9a2b015a4aa72efac72f870d43aa24b3b060
jordnkr <jordnkr408@gmail.com>
2018-01-07 02:34:49 -0600

    Continuing modal work
    Adding more  modal content

--- js/bugs.js
+++ js/bugs.js
@@ -2,4 +2,14 @@ jQuery(document).ready(function($) {
     $('#bugsTable').DataTable( {
         paging: false
     });
+
+    $('#confirmDeleteModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var bugId = button.data('bug'); // Extract info from data-* attributes
+        //var bugs = MAKE CALL TO GET BUGS // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+
+        // Update the modal's content
+        var modal = $(this);
+        modal.find('#modalConfirmNumber').text(bugId);
+    });
 });
--- js/dailyResults.js
+++ js/dailyResults.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function($) {
 
         // Update the modal's content
         var modal = $(this);
-        modal.find('.modal-title').text('Test - ' + testName);
+        modal.find('.modal-title').text(testName);
         modal.find('#modalTestClass').text(testClass);
         modal.find('#modalTestNamespace').text(namespace);
     });
@@ -21,12 +21,23 @@ jQuery(document).ready(function($) {
         var button = $(event.relatedTarget); // Button that triggered the modal
         var testName = button.data('testname');
         var result = button.data('result');
+        var machine = button.data('machine');
         var executionTime = button.data('execution');
+
+        var badgeColor = "";
+        if (result == 'Passed') {
+            badgeColor = "green";
+        } else if (result == 'Failed') {
+            badgeColor = "red";
+        } else {
+            badgeColor = "yellow";
+        }
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
         var modal = $(this);
-        modal.find('.modal-title').text('Test - ' + testName);
-        modal.find('#modalResult').text(result);
+        modal.find('.modal-title').text(testName);
+        modal.find('#modalResult').text(result).removeClass().addClass(badgeColor + " customBadge");
+        modal.find('#modalMachine').text(machine);
         modal.find('#modalTestExecutionTime').text(executionTime);
     });
 });
