a036c99ac9e12b943eaee345253267bb4ca9c0d7
jordnkr <jordnkr408@gmail.com>
2018-02-01 22:44:07 -0600

    Finishing navigation hookups
    Hooking up navbar URLs to use real ids instead of mockups.

--- resoluteqa/urls.py
+++ resoluteqa/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     # ex: /resoluteqa/
     path('', views.index, name='index'),
     # ex: /resoluteqa/project/2/summary
-    path('project/<int:project_id>/summary', views.summary, name='summary'),
+    path('project/<int:projenv_id>/summary', views.summary, name='summary'),
     # ex: /resoluteqa/suite/3/dailyresults
     path('suite/<int:suite_id>/dailyresults', views.dailyresults, name='dailyresults'),
     # ex: /resoluteqa/suite/3/bugs
-    path('suite/<int:suite_id>/bugs', views.bugs, name='bugs'),
+    path('project/<int:projenv_id>/bugs', views.bugs, name='bugs'),
 ]
--- resoluteqa/views.py
+++ resoluteqa/views.py
@@ -1,17 +1,35 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from .models import Environment, Project, Suite
+from .models import Environment, Project, Suite, ProjectEnvironment
 
 def index(request):
     project_list = Project.objects.all()
     context = {'project_list': project_list}
     return render(request, 'resoluteqa/index.html', context)
 
-def summary(request, project_id):
-    return render(request, 'resoluteqa/summary.html')
+def summary(request, projenv_id):
+    projectenvironment = get_object_or_404(ProjectEnvironment, pk=projenv_id)
+    suite_list = Suite.objects.filter(project_environment_id=projenv_id)
+    context = {
+        'projectenvironment': projectenvironment,
+        'suite_list': suite_list
+    }
+    return render(request, 'resoluteqa/summary.html', context)
 
 def dailyresults(request, suite_id):
-    return render(request, 'resoluteqa/dailyresults.html')
+    suite = get_object_or_404(Suite, pk=suite_id)
+    suite_list = Suite.objects.filter(project_environment_id=suite.project_environment.id)
+    context = {
+        'suite': suite,
+        'suite_list': suite_list
+    }
+    return render(request, 'resoluteqa/dailyresults.html', context)
 
-def bugs(request, suite_id):
-    return render(request, 'resoluteqa/bugs.html')
+def bugs(request, projenv_id):
+    projectenvironment = get_object_or_404(ProjectEnvironment, pk=projenv_id)
+    suite_list = Suite.objects.filter(project_environment_id=projenv_id)
+    context = {
+        'projectenvironment': projectenvironment,
+        'suite_list': suite_list
+    }
+    return render(request, 'resoluteqa/bugs.html', context)
